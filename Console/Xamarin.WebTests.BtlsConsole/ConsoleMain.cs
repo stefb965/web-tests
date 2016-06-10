d9177b67f21ba5d43e7e2701975124a3772e7498
Martin Baulig <martin.baulig@xamarin.com>
2016-06-10 23:45:25 +0200

    Add Xamarin.WebTests.BtlsConsole as console test runner for Blts.

---
+++ Console/Xamarin.WebTests.BtlsConsole/ConsoleMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.AsyncTests;
+using Xamarin.AsyncTests.Console;
+using Mono.Btls.Tests;
+using Mono.Btls.TestProvider;
+using Xamarin.WebTests.MonoTestFramework;
+using Xamarin.WebTests.TestProvider;
+using Xamarin.WebTests.MonoTests;
+
+[assembly: AsyncTestSuite (typeof (BoringTlsTestFeatures), true)]
+[assembly: AsyncTestSuite (typeof (MonoWebTestFeatures), true)]
+
+namespace Xamarin.WebTests.BtlsConsole
+{
+	class ConsoleMain
+	{
+		public static void Main (string[] args)
+		{
+			DependencyInjector.RegisterAssembly (typeof (ConsoleMain).Assembly);
+			DependencyInjector.RegisterAssembly (typeof (WebDependencyProvider).Assembly);
+			DependencyInjector.RegisterAssembly (typeof (MonoTestFrameworkDependencyProvider).Assembly);
+			DependencyInjector.RegisterAssembly (typeof (BoringTlsDependencyProvider).Assembly);
+
+			Program.Run (typeof (ConsoleMain).Assembly, args);
+		}
+	}
+}
