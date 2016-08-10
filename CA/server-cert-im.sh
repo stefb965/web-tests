ca48670511b66c157a906c791b891d2bd2b39d72
Martin Baulig <martin.baulig@xamarin.com>
2016-08-10 16:08:37 +0200

    Recreate certificate.

---
+++ CA/server-cert-im.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+openssl req -config server-cert-im.conf -days 3650 -newkey rsa:4096 -keyout server-cert-im.key -out server-cert-im.req
+openssl ca -batch -config openssl.conf -cert Hamiller-Tube-IM.pem -keyfile Hamiller-Tube-IM.key -key monkey -extfile server-cert-im.conf -extensions server_exts -out server-cert-im.pem -in server-cert-im.req
+openssl pkcs12 -export -passout pass:monkey -out server-cert-im.pfx -inkey server-cert-im.key -in server-cert-im.pem -certfile Hamiller-Tube-IM.pem
+
