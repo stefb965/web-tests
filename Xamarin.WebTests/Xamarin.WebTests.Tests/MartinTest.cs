4673c8ff55affb4bd98b90c57f4c484bb8a02d80
Martin Baulig <martin.baulig@xamarin.com>
2016-08-10 17:19:53 +0200

    Cleanup.

--- Xamarin.WebTests/Xamarin.WebTests.Tests/MartinTest.cs
+++ Xamarin.WebTests/Xamarin.WebTests.Tests/MartinTest.cs
@@ -113,26 +113,6 @@ namespace Xamarin.WebTests
 		{
 			await runner.Run (ctx, cancellationToken);
 		}
-
-		[Work]
-		[AsyncTest]
-		[ConnectionTestCategory (ConnectionTestCategory.MartinTest)]
-		public async Task TestMartinHttpWorking (TestContext ctx, CancellationToken cancellationToken,
-			[ConnectionTestProvider ("DotNet:DotNet")] ConnectionTestProvider provider,
-			HttpsTestParameters parameters, HttpsTestRunner runner)
-		{
-			await runner.Run (ctx, cancellationToken);
-		}
-
-		[Work]
-		[AsyncTest]
-		[ConnectionTestCategory (ConnectionTestCategory.MartinTest)]
-		public async Task TestMartinHttp (TestContext ctx, CancellationToken cancellationToken,
-			[ConnectionTestProvider ("DotNet:DotNet")] ConnectionTestProvider provider,
-			HttpsTestParameters parameters, HttpsTestRunner runner)
-		{
-			await runner.Run (ctx, cancellationToken);
-		}
 	}
 }
 
